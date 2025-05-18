6d76768c3661188c63143e3932af3ab7a1c6363b
GitGaurav72 <gaurav2791997@gmail.com>
2025-05-18 17:23:36 +0530

    build commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+const routes: Routes = [
+  // your existing routes
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+})
+export class AppRoutingModule { }
